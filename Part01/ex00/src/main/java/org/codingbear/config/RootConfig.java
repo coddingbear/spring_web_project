5101eb3148e547ac10a65934311373d917a50945
Nullgom <surfwon@gmail.com>
2018-10-23 20:17:51 +0900

    03 스프링과 Oracle Database 연동

--- Part01/ex00/src/main/java/org/codingbear/config/RootConfig.java
+++ Part01/ex00/src/main/java/org/codingbear/config/RootConfig.java
@@ -1,7 +1,11 @@
 package org.codingbear.config;
 
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * RootConfig 클래스
@@ -9,5 +13,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages= {"org.codingbear.sample"})
 public class RootConfig {
-
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("bookpass");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
+	
 }

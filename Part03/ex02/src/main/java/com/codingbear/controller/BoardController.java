88765ba9c107cc9779287a26b8927d65c16b7446
Nullgom <surfwon@gmail.com>
2018-10-26 16:34:41 +0900

    10 프리젠테이션(웹) 계층의 CRUD 구현 - Java 설정

---
+++ Part03/ex02/src/main/java/com/codingbear/controller/BoardController.java
@@ -0,0 +1,65 @@
+package com.codingbear.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.codingbear.domain.BoardVO;
+import com.codingbear.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+// 10 프리젠테이션(웹) 계층의 CRUD 구현 - Cotroller 구현
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list ..... ");
+		model.addAttribute("list", service.getList());
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("register: " + board);
+		service.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		log.info("/get .....");
+		model.addAttribute("board", service.get(bno));
+		
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify: " + board);
+		
+		if (service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove..... " + bno);
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+
+}
---
+++ Part03/ex02/src/test/java/com/codingbear/controller/BoardControllerTests.java
@@ -0,0 +1,85 @@
+package com.codingbear.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration // Controller 테스트를 위해 추가 - ServletContext를 이용하기 위해서..
+@ContextConfiguration(classes = {
+		com.codingbear.config.RootConfig.class,
+		com.codingbear.config.ServletConfig.class})
+@Log4j
+public class BoardControllerTests {
+	@Setter(onMethod_ = @Autowired)
+	private WebApplicationContext ctx;
+	
+	// 가짜 MVC 생성 - 가짜로 URL과 파라미터등을 부라우저에서 마냥 사용해서 Controller를 테스트할 수 있음.
+	private MockMvc mockMvc; 
+	
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void testList() throws Exception {
+		log.info( // MockMvcRequestBuilders 라는 존재를 이용해서 GET 방식의 호출을 사용합니다.
+			mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());
+	}
+	
+	@Test
+	public void testRegister() throws Exception {
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+				.param("title", "테스트 새글 제목")
+				.param("content", "테스트 새글 내용")
+				.param("writer", "user00")).andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testGet() throws Exception {
+		log.info(mockMvc.perform(MockMvcRequestBuilders
+					.get("/board/get")
+					.param("bno", "2"))
+					.andReturn()
+					.getModelAndView().getModelMap());
+	}
+	
+	@Test
+	public void testModify() throws Exception {
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/modify")
+				.param("bno", "1")
+				.param("title",  "수정된 테스트 새글 제목")
+				.param("content", "수정된 테스트 새글 제목")
+				.param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testRemove() throws Exception {
+		// 삭제전 데이터베이스에 게시물 번호 확인할 것
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove")
+				.param("bno",  "25"))
+				.andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+	}
+}

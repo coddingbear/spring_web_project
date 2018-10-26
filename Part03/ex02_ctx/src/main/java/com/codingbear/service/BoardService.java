31959cf5e13bfa245dbcf60d0e95d593279d0c27
Nullgom <surfwon@gmail.com>
2018-10-26 14:56:48 +0900

    09 비즈니스 계층 - XML설정 사용

---
+++ Part03/ex02_ctx/src/main/java/com/codingbear/service/BoardService.java
@@ -0,0 +1,42 @@
+package com.codingbear.service;
+
+import java.util.List;
+
+import com.codingbear.domain.BoardVO;
+
+// 9.1 비즈니스 계층 설정
+public interface BoardService {
+	
+	/**
+	 * 게시글 등록
+	 * @param board
+	 */
+	public void register(BoardVO board);
+	
+	/**
+	 * 게시글 보기
+	 * @param bno
+	 * @return
+	 */
+	public BoardVO get(Long bno);
+	
+	/**
+	 * 게시글 수정
+	 * @param board
+	 * @return
+	 */
+	public boolean modify(BoardVO board);
+	
+	/**
+	 * 게시글 삭제
+	 * @param bno
+	 * @return
+	 */
+	public boolean remove(Long bno);
+	
+	/**
+	 * 게시글 목록
+	 * @return
+	 */
+	public List<BoardVO> getList();
+}
